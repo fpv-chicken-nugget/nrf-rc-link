b5bb4f84c4b472af46456d77459a23beed5c8087
Nahshon Weissberg <nahshonweissberg@gmail.com>
2026-01-26 14:48:35 -0500

    init submodule

---
+++ nrf_rc_link.c
@@ -0,0 +1,7 @@
+#include "nrf_rc_link.h"
+
+#include <stdio.h>
+
+void hello(void) {
+    printf("Hello, World!\n");
+}
---
+++ nrf_rc_link.h
@@ -0,0 +1,6 @@
+#ifndef NRF_RC_LINK_LIBRARY_H
+#define NRF_RC_LINK_LIBRARY_H
+
+void hello(void);
+
+#endif // NRF_RC_LINK_LIBRARY_H
