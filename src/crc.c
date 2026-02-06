0a30846baaa8e15eb85c07dbf62b3810efecda5a
Nahshon Weissberg <nahshonweissberg@gmail.com>
2026-02-06 11:38:39 -0500

    modified crc according to 8 bit header skeleton

--- src/crc.c
+++ src/crc.c
@@ -1,16 +1,29 @@
+/**
+* @file crc.c
+ * @brief CRC-8 implementation
+ */
+
 #include "crc.h"
 
-uint8_t rc_crc8(const uint8_t *data, size_t len) {
-    uint8_t crc = 0xFF;
+/* CRC-8-CCITT polynomial: x^8 + x^2 + x + 1 */
+#define CRC8_POLYNOMIAL 0x07
+#define CRC8_INIT       0x00
+
+uint8_t rc_crc8_calculate(const uint8_t *data, size_t len)
+{
+    uint8_t crc = CRC8_INIT;
+
     for (size_t i = 0; i < len; i++) {
         crc ^= data[i];
-        for (uint8_t j = 0; j < 8; j++) {
+
+        for (uint8_t bit = 0; bit < 8; bit++) {
             if (crc & 0x80) {
-                crc = (crc << 1) ^ 0x07;  // CRC-8-CCITT polynomial
+                crc = (crc << 1) ^ CRC8_POLYNOMIAL;
             } else {
-                crc <<= 1;
+                crc = (crc << 1);
             }
         }
     }
+
     return crc;
 }
